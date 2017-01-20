a3ac64dd8a14ef37212f13e0fbe7bb6d47928df0
Michael Tambornino <michael.tambornino@gmail.com>
2017-01-20 16:02:00 -0600

    added patients

--- app/controllers/patientCtrl.js
+++ app/controllers/patientCtrl.js
@@ -1,4 +1,9 @@
 app
   .controller('patientCtrl', function ($scope, dataFactory) {
 
+    dataFactory.getData()
+      .then((info) => {
+        $scope.doctors = info.doctors
+        $scope.patients = info.patients
+      })
   })
